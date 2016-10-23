2c74950687cc234d2725925217a761e151f83dff
Vlad-de-gol <Vlad-de-gol@ex.ua>
2016-10-23 21:59:24 +0300

    First gulp 0.0.1

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+ 
+gulp.task('default', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss("mainplus.css"))
+});
+
+
+
+
